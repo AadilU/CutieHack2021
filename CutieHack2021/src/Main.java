a05d25fce6702522681c330ee2c715b85fdc7b7b
ashok <ashok@DESKTOP-FTDGPAV.attlocal.net>
2021-11-06 10:38:47 -0700

    commiting GUI class, and removing uneccasary libraries from main

--- CutieHack2021/src/Main.java
+++ CutieHack2021/src/Main.java
@@ -1,9 +1,4 @@
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 public class Main {
 	public static void main(String args[]) throws IOException, InterruptedException{
 		Movie test = new Movie(); 
