83532f5f4ffb902377f05009acaf4a5722a3e6ba
ashok <ashok@DESKTOP-FTDGPAV.attlocal.net>
2021-11-06 10:17:18 -0700

    User input string prob works

--- CutieHack2021/src/Main.java
+++ CutieHack2021/src/Main.java
@@ -5,21 +5,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException, InterruptedException {
-		int i = 0;
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=Attack%20on%20Titan&country=us"))
-				.header("x-rapidapi-host", "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com")
-				.header("x-rapidapi-key", "99733d641cmshb68877f62552883p18b4a8jsna783d2c52835")
-				.method("GET", HttpRequest.BodyPublishers.noBody())
-				.build();
-		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-		String S = response.body();  
-		/*
-		System.out.println(response.body());
-		System.out.println("HEY");
-		*/
+	public static void main(String args[]) throws IOException, InterruptedException{
+		Movie test = new Movie(); 
+		test.settingUserMovie();
+		System.out.println(test.response.body());
 	}
-
 }
--- CutieHack2021/src/Movie.java
+++ CutieHack2021/src/Movie.java
@@ -1,21 +1,30 @@
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.*;  
 public class Movie {
 	String user_m;
 	HttpRequest request;
-	public void setUserMovie() 
+	HttpResponse<String> response;
+	public void settingUserMovie() throws IOException, InterruptedException 
 	{
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter in the movie or TV Show you would like to search");
 		user_m = sc.nextLine(); 
 		setUserApi();
 	}
+	public void setUserMovie(String Program_s) throws IOException, InterruptedException 
+	{ 
+		user_m = Program_s;
+		setUserApi();
+	}
 	public String getUserMovie() 
 	{ 
 		return user_m;
 	}
-	private void setUserApi() 
+	private void setUserApi() throws IOException, InterruptedException 
 	{ 
 		int temp, length = user_m.length();
 		while (user_m.contains(" "))
@@ -29,7 +38,7 @@ public class Movie {
 				.header("x-rapidapi-host", "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com")
 				.header("x-rapidapi-key", "99733d641cmshb68877f62552883p18b4a8jsna783d2c52835")
 				.method("GET", HttpRequest.BodyPublishers.noBody())
-				.build();
-		
+				.build(); 
+		 response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 	}
 }
