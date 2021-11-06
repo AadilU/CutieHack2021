6a8ca28f9a5b1b54cf8d4054df5f0e108471f586
aadil <aadil@LAPTOP-I1U8FLKS.lan>
2021-11-06 14:03:49 -0700

    Added movie entry

--- CutieHack2021/src/Main.java
+++ CutieHack2021/src/Main.java
@@ -9,26 +9,36 @@ import org.json.simple.JSONObject;
 
 public class Main {
 	public static void main(String args[]) throws IOException, InterruptedException, ParseException{
-		ArrayList<String> platformNames = new ArrayList();
 		Movie test = new Movie(); 
 		test.settingUserMovie();
 		parse1(test.response.body());
 	}
 	
+	//Parse per 1 search
 	public static void parse1(String body) throws ParseException {
 		JSONParser parser = new JSONParser();
 		Object o = parser.parse(body);
 		JSONObject jObject = (JSONObject)o;
 		JSONArray jArray = (JSONArray) jObject.get("results");
 		
+		String movieName = "";
+		ArrayList<String> movieStream = new ArrayList();
+		
 		for(int i = 0;i < jArray.size();i++) {
+				MovieEntry addMovie;
+			
 				JSONObject jObject1 = (JSONObject) jArray.get(i);
+				
+				movieName = (String) jObject1.get("name");
+				
 				JSONArray jArray2 = (JSONArray) jObject1.get("locations");
 					for(Object n:jArray2) {
 						JSONObject jObject3 = (JSONObject)n;
 						String name = (String) jObject3.get("display_name");
-						System.out.println(name);
+						movieStream.add(name);
 					}
+					addMovie = new MovieEntry(movieName, movieStream);
+					movieStream.clear();
 			}
 		}
 	}
---
+++ CutieHack2021/src/MovieEntry.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class MovieEntry {
+
+	String movieName;
+	ArrayList<String> services;
+	
+	MovieEntry(String name, ArrayList<String> streamList) {
+		movieName = name;
+		services = streamList;
+		
+		System.out.println(name + " available on:");
+		for(int i = 0;i < streamList.size();i++) {
+			System.out.println(streamList.get(i));
+		}
+		System.out.println("\n");
+	}
+	
+}
