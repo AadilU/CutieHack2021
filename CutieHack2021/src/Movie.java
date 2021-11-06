c2b4f5995946cc5da1d96b6ec6d7dcd7e5c640e6
ashok <ashok@DESKTOP-FTDGPAV.attlocal.net>
2021-11-06 09:42:26 -0700

    making setUserMovie and getUserMovie member functions

--- CutieHack2021/src/Movie.java
+++ CutieHack2021/src/Movie.java
@@ -1,12 +1,15 @@
 import java.util.*;  
 public class Movie {
-	public String getUserMovie() 
+	String user_m;
+	public void setUserMovie() 
 	{
-		String user_m;
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter in the movie or TV Show you would like to search");
-		user_m = sc.nextLine();
-		return user_m; 
+		user_m = sc.nextLine(); 
 		
+	}
+	public String getUserMovie() 
+	{ 
+		return user_m;
 	}
 }
