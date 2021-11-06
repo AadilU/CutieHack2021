8c060cd3e20aeed76e9e6dcd01bd6ef132d6fffd
aadil <aadil@LAPTOP-I1U8FLKS.lan>
2021-11-06 11:58:55 -0700

    JSON Parse

--- CutieHack2021/src/Main.java
+++ CutieHack2021/src/Main.java
@@ -1,10 +1,27 @@
 import java.io.IOException;
-import org.json.simple.*;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Main {
-	public static void main(String args[]) throws IOException, InterruptedException{
+	public static void main(String args[]) throws IOException, InterruptedException, ParseException{
 		Movie test = new Movie(); 
 		test.settingUserMovie();
-		System.out.println(test.response.body());
+		parse1(test.response.body());
+	}
+	
+	public static void parse1(String body) throws ParseException {
+		JSONParser parser = new JSONParser();
+		Object o = parser.parse(body);
+		JSONObject jObject = (JSONObject)o;
+		JSONArray jArray = (JSONArray) jObject.get("results");
+		
+		for(Object n:jArray) {
+			JSONObject jNum = (JSONObject) n;
+			String num = (String) jNum.get("id");
+			System.out.println(num);
+		}
 	}
 }
