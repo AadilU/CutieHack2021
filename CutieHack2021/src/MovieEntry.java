20610034d50872413f2ab066497e35ff40df99d6
Ashokcanchi <93831758+Ashokcanchi@users.noreply.github.com>
2021-11-06 19:28:04 -0700

    Update MovieEntry.java

--- CutieHack2021/src/MovieEntry.java
+++ CutieHack2021/src/MovieEntry.java
@@ -5,13 +5,13 @@ public class MovieEntry {
 	String movieName;
 	ArrayList<String> services;
 	
-	MovieEntry(String name, ArrayList<String> streamList, String icon) {
+	MovieEntry(String name, ArrayList<String> movieStream, String icon) {
 		movieName = name;
-		services = streamList;
+		services = new ArrayList<String>();
 		
 		System.out.println(name + " available on:");
-		for(int i = 0;i < streamList.size();i++) {
-			System.out.println(streamList.get(i));
+		for(int i = 0;i < movieStream.size();i++) {
+			services.add(movieStream.get(i));
 		}
 		System.out.println(icon);
 		System.out.println("\n");
